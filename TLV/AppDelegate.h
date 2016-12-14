7a9b681ee76152a6bccc4e41300b99b405e12c2a
lawrence <law@xqopen.com>
2016-12-14 10:40:03 +0800

    Initial Commit

---
+++ TLV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TLV
+//
+//  Created by lawrence on 16/12/14.
+//  Copyright © 2016年 李辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TLV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TLV
+//
+//  Created by lawrence on 16/12/14.
+//  Copyright © 2016年 李辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
